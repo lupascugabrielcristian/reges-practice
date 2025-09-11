1935d770a246152bab3224fb0b3b9e10547ff8f5
Cristian Lupascu <lupascugabrielcristian@yahoo.com>
2025-09-11 15:18:25 +0300

    suspendare contract

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ document.getElementById('authenticateForm').addEventListener('submit', async (e)
 });
 
 // PROFILE
-const myAuthToken = `eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ0QWZKOTk2XzhaZkUzVVczSHpBcGgzbjRaREVLcDdyaWE0SlVHLWNoN2FnIn0.eyJleHAiOjE3NTc1ODU1MzMsImlhdCI6MTc1NzU4MzczMywianRpIjoiYmM2MzJiOWMtOWI1My00YWNlLWIxMTgtNjRlYWZlNTYzYzQ4IiwiaXNzIjoiaHR0cHM6Ly9zc28uZGV2Lmluc3BlY3RpYW11bmNpaS5vcmcvcmVhbG1zL0FQSSIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiIxOGJlNTE1Yi1hNjdmLTQ2NjAtYjZlYS1mNjM0OWJjMThiMjciLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJyZWdlcy1hcGkiLCJzaWQiOiIzMTgzM2FiNi04ZTcyLTQ2MjctYjMyMi1lOTUxYTk2YjBjNDAiLCJhY3IiOiIxIiwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtYXBpIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6IkNSSVNUSUFOIExVUEFTQ1UgQklOQVJZIEZVU0lPTiBTLlIuTC4iLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiIyMzJmN2RmYy0wYzM2LTRjMjMtYWVhNy0yNzE0MTUzNzJjZGUiLCJnaXZlbl9uYW1lIjoiQ1JJU1RJQU4gTFVQQVNDVSIsImF1dGhvcklkIjoiMjMyZjdkZmMtMGMzNi00YzIzLWFlYTctMjcxNDE1MzcyY2RlIiwiZmFtaWx5X25hbWUiOiJCSU5BUlkgRlVTSU9OIFMuUi5MLiIsImVtYWlsIjoiMjMyZjdkZmMtMGMzNi00YzIzLWFlYTctMjcxNDE1MzcyY2RlQGluc3BlY3RpYW11bmNpaS5ybyJ9.r-6-jFo2-Tq5t6SXfBPtPfsnj5V3rbEUj_BMzxwgT7iB8KVfOMA_W5DduT0RMo6H-vtkS8T8qLg8CXG6rF2jxeCKhPDI39JgUDAdKqxegt15ovNokY1zsXHaShBGeN_UVsg-glfdzOhJe946MuaQrrQDnhJuJS7fQW75c0NKhhMt7b296e4oNT1DS-bZD6jGKkmbnGiHrHUUsYi2Lhccnzs2wwVZKbq0bHZMEhxiEKLIqz3o2UGGDQaqLdsVWKGSRvrXCBEeVarl0KurTE4qbI-OBYCtbUP84OiisQ6ETYek8Yz8ksyr3ibY4Qe4QZpkm_GPdjjLHoU9Ocna8-WEVw`
+const myAuthToken = `eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ0QWZKOTk2XzhaZkUzVVczSHpBcGgzbjRaREVLcDdyaWE0SlVHLWNoN2FnIn0.eyJleHAiOjE3NTc1OTQyMjMsImlhdCI6MTc1NzU5MjQyMywianRpIjoiZDVhYmRhYTMtYjM0Ni00MGFkLThjYjgtMmYwYTU1N2YwZWU4IiwiaXNzIjoiaHR0cHM6Ly9zc28uZGV2Lmluc3BlY3RpYW11bmNpaS5vcmcvcmVhbG1zL0FQSSIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiIxOGJlNTE1Yi1hNjdmLTQ2NjAtYjZlYS1mNjM0OWJjMThiMjciLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJyZWdlcy1hcGkiLCJzaWQiOiI3M2E0OGNlZS1kNTNkLTQ3MDUtYmI0My1kYjg1NGQ3MDgyYzYiLCJhY3IiOiIxIiwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImRlZmF1bHQtcm9sZXMtYXBpIiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6IkNSSVNUSUFOIExVUEFTQ1UgQklOQVJZIEZVU0lPTiBTLlIuTC4iLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiIyMzJmN2RmYy0wYzM2LTRjMjMtYWVhNy0yNzE0MTUzNzJjZGUiLCJnaXZlbl9uYW1lIjoiQ1JJU1RJQU4gTFVQQVNDVSIsImF1dGhvcklkIjoiMjMyZjdkZmMtMGMzNi00YzIzLWFlYTctMjcxNDE1MzcyY2RlIiwiZmFtaWx5X25hbWUiOiJCSU5BUlkgRlVTSU9OIFMuUi5MLiIsImVtYWlsIjoiMjMyZjdkZmMtMGMzNi00YzIzLWFlYTctMjcxNDE1MzcyY2RlQGluc3BlY3RpYW11bmNpaS5ybyJ9.ONrrmoqNiNyPLxONJNF_lYPprfQGvqxbjGDq1WTVh6K19ybfql20vtUcoXjxN0Deyo6Hl2Td4T7v59HJVjGmHVXZm8QaR1qWRKnG3yUMBzrFhtt7i00FC0n_l4sy28kClRtOCPhIgAyMHmj2TOQ0zpKBSfMYiVvIj3Bj-ZTbCFQfcqzLP2FD4CTzUxOaDFORYzbokn6KYZ6Y78_ipWfWuvgeDPBNVk-st3eVfnxOwQ1MzpiQDnsRkUOZbg-1YOSanDsM21SZ0KjWS_0LPO9euaiN4ffDx6Y3ftPHd6f5KYIxfIHNzJaVoN-rrzNVO-TWKenMo_mPEO65u-R7qiC7DA`
 document.getElementById('result-auth').textContent = myAuthToken;
 document.getElementById('profileForm').addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -186,6 +186,54 @@ document.getElementById('modificareContractForm').addEventListener('submit', asy
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({ authToken, field1, field2 })
+        });
+        
+        try {
+            let dataJson = await response.clone().json();
+
+            if (!dataJson ) {
+                resultDiv.innerHTML = `Server errr: ${dataJson }`;
+            } 
+            else {
+                if (dataJson.status == 200) {
+                    resultDiv.innerHTML = JSON.stringify(dataJson.response)
+                } else if (dataJson.status == 2000) {
+                    resultDiv.innerHTML = 'CERERE TRIMISA </br>'+ dataJson.response;
+                }
+                else if (dataJson.status == 400) {
+                    resultDiv.innerHTML = 'BAD REQUEST </br>'+ dataJson.response;
+                }
+                else if (dataJson.status == 401) {
+                    resultDiv.innerHTML = '401 - Tokenul a exirat';
+                }
+                else {
+                    resultDiv.innerHTML = JSON.stringify(dataJson);
+                }
+            }
+        } catch (err_p) {
+            resultDiv.innerHTML = await response.text();
+        }
+
+    } catch (error) {
+        resultDiv.innerHTML = `Error: ${error.message}`;
+    }
+});
+
+// SUSPENDARE CONTRACT
+document.getElementById('suspendareContractForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    
+    const authToken = document.getElementById('result-auth').textContent;
+    const field1 = document.getElementById('field10').value;
+    const resultDiv = document.getElementById('result-suspendare-contract');
+    
+    try {
+        const response = await fetch('api/suspendare-contract.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ authToken, field1 })
         });
         
         try {
